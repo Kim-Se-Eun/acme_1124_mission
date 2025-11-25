c00c5ee7ab9dadf784867f16a023f1662a778b79
Kim-Se-Eun <yappay464@gmail.com>
2025-11-25 17:02:36 +0900

    후기 슬라이드 Js 추가

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 const btt = document.querySelector('#btt');
+const testimonial = document.querySelector('.testimonial');
+const slideContainer = testimonial.querySelector('ul');
+const slides = slideContainer.querySelectorAll('li');
+let currentIdx = 0;
+const prevBtn = testimonial.querySelector('.prev');
+const nextBtn = testimonial.querySelector('.next');
+const slideCount = slides.length;
 
 window.addEventListener('scroll',()=>{
     let scrAmt = window.scrollY;
@@ -15,4 +22,24 @@ btt.addEventListener('click',(e)=>{
         top:0,
         behavior:'smooth'
     });
+});
+
+moveSlide(0);
+
+function moveSlide(idx){
+    slideContainer.style.left = -idx*100+'%';
+    currentIdx = idx;
+    for(let slide of slides){
+        slide.classList.remove('active');
+    }
+    slides[idx].classList.add('active');
+}
+
+prevBtn.addEventListener('click',()=>{
+    let nextIdx = (currentIdx - 1 + slideCount)%slideCount;
+    moveSlide(nextIdx);
+});
+nextBtn.addEventListener('click',()=>{
+    let nextIdx = (currentIdx + 1)%slideCount;
+    moveSlide(nextIdx);
 });
